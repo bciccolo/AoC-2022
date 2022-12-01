e8e63e4e2d3e08e46cb2f136c49a237ed56041eb
Brian Ciccolo <brian@passingfocus.net>
2022-12-01 12:58:44 -0500

    Day 1 (both parts)

---
+++ day1.py
@@ -0,0 +1,30 @@
+def part1():
+    print("Part 1: " + str(totals[0]))
+
+def part2():
+    print("Part 1: " + str(totals[0] + totals[1] + totals[2]))
+
+
+def loadData():
+    global totals
+
+    file = open('day1.dat', 'r')
+    lines = file.readlines()
+
+    runningTotal = 0
+    totals = []
+
+    for line in lines:
+        if (line.strip() == ''):
+            totals.append(runningTotal)
+            runningTotal = 0
+        else:
+            number = int(line.strip())
+            runningTotal += number
+
+    totals.sort(reverse = True)
+
+
+loadData()
+part1()
+part2()
